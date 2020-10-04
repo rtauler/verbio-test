43f2036473b8cb87daa52c869ca94c1647ca205a
rtauler <rtauler@gmail.com>
2020-10-04 22:13:30 +0200

    added routing module and definitions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule} from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,23 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      [
+        {
+          path : '',
+          component: HomeComponent
+        },
+        {
+          path: 'login',
+          component: LoginComponent
+        },
+        {
+          path: 'admin',
+          component: AdminComponent
+        }
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  getUserDetails(){
+    //post details to APi Server and return if correct
+  }
+
+}

5ec7bf19b4a64df3f8e316890bdf66d2f3f066f1
rtauler <rtauler@gmail.com>
2020-10-06 15:14:09 +0200

    text well displayed, cats image showing

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -20,39 +20,40 @@ export class AdminComponent implements OnInit {
   
   text:string
   
+  userMessages: any[] = [];
+  
   botMessages: any[] = [];
   
   constructor(private http: HttpClient) {}
   
   postData(){
-    
-    
     this.http.post('http://0.0.0.0:5556/sendMessage',{
-      text:this.text
-    }).toPromise().then((data:any)=>{
-      console.log(data)
-    })
-    
+    text:this.text
+  }).toPromise().then(
+    (data:any) => {
+      console.log(data.response)
+      this.userMessages = data.response;
+  })
+  
+}
+
+ngOnInit(): void {
+  this.http.get('http://0.0.0.0:5556/getWelcomeMessage')
+  .subscribe(
+    (data: any) => {
+      this.botMessages = data.response; 
+      // Where you find the array res.data or res.data.data
+      
+      
+    },
+    error => {
+      alert("ERROR");
+    });      
   }
   
-  ngOnInit(): void {
-    this.http.get('http://0.0.0.0:5556/getWelcomeMessage')
-    .subscribe(
-      (data: any) => {
-        this.botMessages = data.response; 
-        // Where you find the array res.data or res.data.data
-        
-        
-      },
-      error => {
-        alert("ERROR");
-      });      
-    }
-    
-    
-  }
   
-  
-  
-  
-  
+}
+
+
+
+
